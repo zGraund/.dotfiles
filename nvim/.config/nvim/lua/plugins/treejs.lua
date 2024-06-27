57f3335c7c394b4beec91958bb40e8b301ce82ad
zGraund <leo.gaud@hotmail.it>
2024-06-28 01:37:10 +0200

    lazy load treejs

--- nvim/.config/nvim/lua/plugins/treejs.lua
+++ nvim/.config/nvim/lua/plugins/treejs.lua
@@ -1,13 +1,15 @@
 return {
 	"Wansmer/treesj",
-	keys = { "<leader>tt", "<leader>tT" },
 	dependencies = { "nvim-treesitter/nvim-treesitter" }, -- if you install parsers with `nvim-treesitter`
+	-- stylua: ignore
+	keys = {
+		{ "<leader>tt", function() require("treesj").toggle() end, { desc = "Toggle split line" } },
+		{ "<leader>tT", function() require("treesj").toggle({ split = { recursive = true } }) end, { desc = "Toggle split line recursive" } },
+	},
 	config = function()
-		require("treesj").setup({ use_default_keymaps = false })
-
-		vim.keymap.set("n", "<leader>tt", require("treesj").toggle, { desc = "Toggle split line" })
-		vim.keymap.set("n", "<leader>tT", function()
-			require("treesj").toggle({ split = { recursive = true } })
-		end, { desc = "Toggle split line recursive" })
+		require("treesj").setup({
+			max_join_length = 180,
+			use_default_keymaps = false,
+		})
 	end,
 }
