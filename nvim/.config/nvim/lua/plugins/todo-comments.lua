ae4c35688dfe29a912d4d895701a7d162dd96387
zGraund <leo.gaud@hotmail.it>
2024-06-28 01:35:52 +0200

    added todo search

--- nvim/.config/nvim/lua/plugins/todo-comments.lua
+++ nvim/.config/nvim/lua/plugins/todo-comments.lua
@@ -1,7 +1,15 @@
 return {
 	-- Highlight todo, notes, etc in comments
 	"folke/todo-comments.nvim",
-	event = "VimEnter",
+	event = "BufEnter",
+	cmd = { "TodoTrouble", "TodoTelescope" },
 	dependencies = { "nvim-lua/plenary.nvim" },
 	opts = {},
+	-- stylua: ignore
+	keys = {
+		{ "]t", function() require("todo-comments").jump_next() end, desc = "Next Todo Comment" },
+		{ "[t", function() require("todo-comments").jump_prev() end, desc = "Previous Todo Comment" },
+		{ "<leader>st", "<cmd>TodoTelescope<cr>", desc = "[S]earch [T]odo" },
+		{ "<leader>sT", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>", desc = "[S]earch Todo/Fix/Fixme" },
+	},
 }
